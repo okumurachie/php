1d2c0ad5e671a6122915530e2ce22a117a681271
okumura chie <neco9145@me.com>
2025-02-10 11:27:22 +0900

    リモートリポジトリの変更

---
+++ src/php02-2/config/cities.php
@@ -0,0 +1,39 @@
+<?php
+$cities=[
+    [
+        'name'=>'シドニー',
+        'time zone'=>'Australia/Sydney',
+        'img'=>'australia.jpg'
+    ],
+    [
+        'name'=>'東京',
+        'time zone'=>'Asia/Tokyo',
+        'img'=>'japan.jpg'
+    ],
+    [
+        'name'=>'上海',
+        'time zone'=>'Asia/Shanghai',
+        'img'=>'china.jpg'
+    ],
+    [
+        'name'=>'モスクワ',
+        'time zone'=>'Europe/Moscow',
+        'img'=>'russia.jpg'
+    ],
+    [
+        'name'=>'ロンドン',
+        'time zone'=>'Europe/London',
+        'img'=>'british.jpg'
+    ],
+    [
+        'name'=>'ヨハネスブルグ',
+        'time zone'=>'Africa/Johannesburg',
+        'img'=>'south-africa.jpg'
+    ],
+    [
+        'name'=>'ニューヨーク',
+        'time zone'=>'America/New_York',
+        'img'=>'america.jpg'
+    ]
+];
+?>
